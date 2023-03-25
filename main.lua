e0ca5ab78fa860696b9b032472b0aed02fa041f6
ave <94792074+Averiias@users.noreply.github.com>
2023-03-25 23:08:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -240,7 +240,7 @@ local function getClosestPlayer()
 end
 
 -- ui creating & handling
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/UI's/Linoria/Source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 Library:SetWatermark("github.com/Averiias")
 
 local Window = Library:CreateWindow("Universal Silent Aim, by Averiias, xaxa, and Stefanuk12")
